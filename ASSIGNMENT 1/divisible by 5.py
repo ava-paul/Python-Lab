001bed6d490bd34669e98bf6a13b813e07952438
Ava Paul <106614026+ava-paul@users.noreply.github.com>
2024-08-01 12:53:23 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/divisible by 5.py
@@ -0,0 +1,6 @@
+#Write a program to check whether a number is divisible by 5 or not.
+n=int(input("Enter a number :"))
+if(n%5==0):
+    print("divisible by 5")
+else:
+    print("not divisible by 5")
---
+++ ASSIGNMENT 1/even_odd positive_negative.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number to check odd or even:"))
+if(n%2==0):
+    print("It is even")
+else:
+    print("It is odd")
+n=int(input("Enter a number to check positive or negative:"))
+if(n<0):
+    print("It is negative")
+else:
+    print("It is positive")
